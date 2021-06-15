794c0724495537184500b2a237013417531e81cd
Funso Joba <42432746+funsojoba@users.noreply.github.com>
2021-06-15 18:13:11 +0100

    added React frontend

--- todo_api/models.py
+++ todo_api/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Todo(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
     title = models.CharField(max_length=250)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
